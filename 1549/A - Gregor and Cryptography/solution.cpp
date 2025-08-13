5a1d70f26dcedba175d13c2c333efb519f4ae0a0
Shashwat Srivastava <shashwat0805@protonmail.com>
2025-08-13 18:38:35 +0530

    Add Gregor and Cryptography [A] from Codeforces

---
+++ 1549/A - Gregor and Cryptography/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h> using namespace std; void solve(){    int P;    cin >> P;    cout << 2 << " " << (P - 1) << '\n';} int main(){    ios::sync_with_stdio(false);    cin.tie(nullptr);    cout.tie(nullptr);     int t;    cin >> t;    while (t--)    {        solve();    }     return 0;}
